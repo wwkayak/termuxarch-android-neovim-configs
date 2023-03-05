a621c23c5ce8ffb65803dcc5219ea5efa4657f31
wwkayak <wwkayak@users.noreply.github.com>
2023-03-05 14:38:51 -0500

    added custom diagnostic open_float() window to nvim-diagnostics.lua

--- lua/nvim-diagnostics.lua
+++ lua/nvim-diagnostics.lua
@@ -2,7 +2,7 @@
 local kopts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>dl', vim.diagnostic.goto_next, kopts)
 vim.keymap.set('n', '<leader>dh', vim.diagnostic.goto_prev, kopts)
-vim.keymap.set('n', '<leader>ds', vim.diagnostic.setloclist, kopts)
+vim.keymap.set('n', '<leader>da', vim.diagnostic.open_float, kopts)
 vim.keymap.set('n', '<leader>dt', "<cmd>Telescope diagnostics<cr>", kopts)
 
 -- Set the icons usedfordebugging to the codicon icons from a patched font
@@ -15,9 +15,10 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
+--
 -- defaults, I like to sort by priority
 -- Need to figure out individual virtual_text icons, based on priority (handlers?)
+
 vim.diagnostic.config({
   virtual_text = true,
   prefix = true,
@@ -25,4 +26,14 @@ vim.diagnostic.config({
   underline = true,
   update_in_insert = false,
   severity_sort = true,
+  float = {
+    border = "rounded",
+    header = "All Diagnostics",
+    scope = "buffer",
+    format = function(diag)
+        local str =
+          "[" .. diag.lnum .. "][" .. diag.col .. "] : " ..  string.sub(diag.message, 1, 30)
+        return str
+      end
+  },
 })
