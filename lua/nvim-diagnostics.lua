9fc75d4355bc842952230e9149e0940eda045c57
wwkayak <wwkayak@users.noreply.github.com>
2023-03-12 11:28:40 -0400

    More diagnostics UI mods, made changes to source code: diagnostics.lua to get icons on virtual text

--- lua/nvim-diagnostics.lua
+++ lua/nvim-diagnostics.lua
@@ -1,3 +1,6 @@
+--
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float({scope = "line", header = "Line Diagnostics"})]]
+--
 -- Diagnosticsn Shortcuts --
 local kopts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>dl', vim.diagnostic.goto_next, kopts)
@@ -9,7 +12,7 @@ vim.keymap.set('n', '<leader>dt', "<cmd>Telescope diagnostics<cr>", kopts)
 -- I had to patch the original Hack font manually to get codicons working.
 -- To insert unicode w/o (copy/paste), ^vuxxxx . So ^vuf835 = 
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Info = " ", Hint = " " }
 
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
@@ -18,9 +21,14 @@ end
 --
 -- defaults, I like to sort by priority
 -- Need to figure out individual virtual_text icons, based on priority (handlers?)
-
 vim.diagnostic.config({
-  virtual_text = true,
+  virtual_text = {
+    format = function(diag)
+      local str = ""
+      return str
+    end,
+    prefix = "",
+  },
   prefix = true,
   signs = true,
   underline = true,
@@ -31,9 +39,12 @@ vim.diagnostic.config({
     header = "All Diagnostics",
     scope = "buffer",
     format = function(diag)
-        local str =
-          "[" .. diag.lnum .. "][" .. diag.col .. "] : " ..  string.sub(diag.message, 1, 30)
-        return str
-      end
+      local str =
+          "[" .. diag.lnum .. "][" .. diag.col .. "] : " .. string.sub(diag.message, 1, 30)
+      return str
+    end,
+    --    ["my_virt_text"] = {
+    --      severity_icon = ""
+    --    },
   },
 })
--- lua/tokyonights-config.lua
+++ lua/tokyonights-config.lua
@@ -1,17 +1,48 @@
 require("tokyonight").setup(
   {
-
     style = "night",
-
-    on_highlights = function(highlights, colors)
+    on_highlights = function(highlights, _)
       highlights.WinSeparator = {
         fg = '#333344'
       }
+      highlights.Comment = {
+        fg = "#445599"
+      }
+      highlights.DiagnosticVirtualTextError = {
+        --bg = "#2d202a",
+        fg = "#eb3b3b",
+      }
+      highlights.DiagnosticError = {
+        --bg = "#2d202a",
+        fg = "#eb3b3b",
+      }
+      highlights.DiagnosticVirtualTextHint = {
+        --bg = "#1a2232",
+        fg = "#f0ef58"
+      }
+      highlights.DiagnosticHint = {
+        --bg = "#1a2232",
+        fg = "#f0ef58"
+      }
+      highlights.DiagnosticVirtualTextInfo = {
+        --bg = "#192b38",
+        fg = "#0dd9f7"
+      }
+      highlights.DiagnosticInfo = {
+        --bg = "#192b38",
+        fg = "#0dd9f7"
+      }
+      highlights.DiagnosticVirtualTextWarn = {
+        --bg = "#3e2a2d",
+        fg = "#f09f68"
+      }
+      highlights.DiagnosticWarn = {
+        --bg = "#3e2a2d",
+        fg = "#f09f68"
+      }
     end,
-
     on_color = function(color)
       TabLine = { bg = "#8888ff", fg = color.fg_gutter }
     end
-
   }
 )
